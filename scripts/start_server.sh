f8ebd8360a6f77dd8308b9ed83b86447be991413
Janhvi Maddeshiya <110676016+JanhviMaddeshiya@users.noreply.github.com>
2024-09-07 11:24:34 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,7 +5,7 @@ cd /opt/myapp
 
 # Start the application
 # Example using a Python web server, replace with your actual start command
-nohup python3 app.py > app.log 2>&1 &
+nohup python3 manage.py runserver 0.0.0.0:8000 > app.log 2>&1 &
 
 # Optionally, you can check if the application is running
 ps aux | grep app.py
