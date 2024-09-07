10456be9b844fc943dda04f8bb66095f29c851fb
Janhvi Maddeshiya <110676016+JanhviMaddeshiya@users.noreply.github.com>
2024-09-07 13:57:29 +0530

    Create post_deploy_cleanup.sh

---
+++ scripts/post_deploy_cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Remove old temporary files
+rm -rf /var/www/myapp/tmp/
+
+# Clear cache (if applicable)
+# Example for clearing a cache directory
+rm -rf /var/www/myapp/cache/
+
+# Any additional cleanup commands can go here
